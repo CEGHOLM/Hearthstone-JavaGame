6aba4a83654c34585865ebbdd7e4a256b53b1fa2
CEGHOLM <christian.mathiasen.egholm@gmail.com>
2024-09-29 19:55:44 +0200

    Iteration 5: EpsilonStoneFactory er lavet.

---
+++ src/main/java/hotstone/variants/epsilonstone/EpsilonStoneFactory.java
@@ -0,0 +1,28 @@
+package hotstone.variants.epsilonstone;
+
+import hotstone.framework.*;
+import hotstone.variants.alphastone.AlphaStoneDeckBuilderStrategy;
+import hotstone.variants.alphastone.AlphaStoneManaStrategy;
+import hotstone.variants.alphastone.AlphaStoneWinnerStrategy;
+
+public class EpsilonStoneFactory implements HotstoneFactory {
+    @Override
+    public ManaProductionStrategy createManaProductionStrategy() {
+        return new AlphaStoneManaStrategy();
+    }
+
+    @Override
+    public WinningStrategy createWinningStrategy() {
+        return new AlphaStoneWinnerStrategy();
+    }
+
+    @Override
+    public HeroStrategy createHeroStrategy() {
+        return new EpsilonStoneHeroStrategy();
+    }
+
+    @Override
+    public DeckBuilderStrategy createDeckBuilderStrategy() {
+        return new AlphaStoneDeckBuilderStrategy();
+    }
+}
