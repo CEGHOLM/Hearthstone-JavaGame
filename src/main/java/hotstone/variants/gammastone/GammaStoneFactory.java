cc7130bde6c46026a9adfc1fa828d2554637314c
CEGHOLM <christian.mathiasen.egholm@gmail.com>
2024-09-29 19:56:45 +0200

    Iteration 5: GammaStoneFactory er lavet.

---
+++ src/main/java/hotstone/variants/gammastone/GammaStoneFactory.java
@@ -0,0 +1,27 @@
+package hotstone.variants.gammastone;
+
+import hotstone.framework.*;
+import hotstone.variants.alphastone.AlphaStoneDeckBuilderStrategy;
+import hotstone.variants.alphastone.AlphaStoneManaStrategy;
+
+public class GammaStoneFactory implements HotstoneFactory {
+    @Override
+    public ManaProductionStrategy createManaProductionStrategy() {
+        return new AlphaStoneManaStrategy();
+    }
+
+    @Override
+    public WinningStrategy createWinningStrategy() {
+        return new GammaStoneWinnerStrategy();
+    }
+
+    @Override
+    public HeroStrategy createHeroStrategy() {
+        return new GammaStoneHeroStrategy();
+    }
+
+    @Override
+    public DeckBuilderStrategy createDeckBuilderStrategy() {
+        return new AlphaStoneDeckBuilderStrategy();
+    }
+}
