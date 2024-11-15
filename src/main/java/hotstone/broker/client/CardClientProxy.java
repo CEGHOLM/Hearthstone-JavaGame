5a2f94711b70afebcb52ef6332b5e8ddeb735d4a
CEGHOLM <christian.mathiasen.egholm@gmail.com>
2024-11-15 15:26:17 +0100

    CardCLientProxy er opsat og klar til at blive implementeret

---
+++ src/main/java/hotstone/broker/client/CardClientProxy.java
@@ -0,0 +1,56 @@
+package hotstone.broker.client;
+
+import frds.broker.ClientProxy;
+import frds.broker.Requestor;
+import hotstone.framework.Card;
+import hotstone.framework.Effect;
+import hotstone.framework.Player;
+
+public class CardClientProxy implements Card, ClientProxy {
+    private String singletonID = "one-card";
+    private final Requestor requestor;
+
+    public CardClientProxy(Requestor requestor) {
+        this.requestor = requestor;
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public int getManaCost() {
+        return 0;
+    }
+
+    @Override
+    public int getAttack() {
+        return 0;
+    }
+
+    @Override
+    public int getHealth() {
+        return 0;
+    }
+
+    @Override
+    public boolean isActive() {
+        return false;
+    }
+
+    @Override
+    public Player getOwner() {
+        return null;
+    }
+
+    @Override
+    public String getEffectDescription() {
+        return "";
+    }
+
+    @Override
+    public Effect getEffect() {
+        return null;
+    }
+}
